a4c07cc40e1cc00fbaa87e0e6bf4a2aa647db098
Daniel Paggiola <dpaggiola@gmail.com>
2025-04-29 22:04:48 -0300

    Clase 12

--- recipe_book/lib/screens/home_screen.dart
+++ recipe_book/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/screens/recipe_detail.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -21,11 +22,12 @@ class HomeScreen extends StatelessWidget {
 
   Future<void> _showBottom(BuildContext context) {
     return showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder:
           (contexto) => Container(
             width: MediaQuery.of(context).size.width,
-            height: 500,
+            height: 600,
             color: Colors.white,
             child: ReciperForm(),
           ),
@@ -33,44 +35,52 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _RecipesCard(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 125,
-        child: Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 125,
-                width: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.network(
-                    'https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg',
-                    fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => RecipeDetail(recipeName: 'Lasagna')),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 125,
+          child: Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  height: 125,
+                  width: 100,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.network(
+                      'https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(width: 26),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Lasagna',
-                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
-                  ),
-                  SizedBox(height: 4),
-                  Container(height: 2, width: 75, color: Colors.orange),
-                  Text(
-                    'Daniel P',
-                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
-                  ),
-                  SizedBox(height: 4),
-                ],
-              ),
-            ],
+                SizedBox(width: 26),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Lasagna',
+                      style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                    ),
+                    SizedBox(height: 4),
+                    Container(height: 2, width: 75, color: Colors.orange),
+                    Text(
+                      'Daniel P',
+                      style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                    ),
+                    SizedBox(height: 4),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -83,10 +93,17 @@ class ReciperForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    final TextEditingController _recipeName = TextEditingController();
+    final TextEditingController _recipeAuthor = TextEditingController();
+    final TextEditingController _recipeIMG = TextEditingController();
+    final TextEditingController _recipeDescription = TextEditingController();
+
     return Padding(
-      padding: EdgeInsets.all(8),
+      padding: EdgeInsets.all(12),
       child: Form(
-        // key: _formKey,
+        key: _formKey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -95,14 +112,86 @@ class ReciperForm extends StatelessWidget {
               style: TextStyle(color: Colors.orange, fontSize: 24),
             ),
             SizedBox(height: 16),
-            _buildTextField(label: 'Recipe Name'),
+            _buildTextField(
+              controller: _recipeName,
+              label: 'Recipe Name',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the name recipe';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 16),
+            _buildTextField(
+              controller: _recipeAuthor,
+              label: 'Author',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the author';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 16),
+            _buildTextField(
+              controller: _recipeIMG,
+              label: 'Image URL',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the image';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 16),
+            _buildTextField(
+              maxLines: 4,
+              controller: _recipeDescription,
+              label: 'Description',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the description';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 16),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pop(context);
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Save Recipe',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildTextField({required String label}) {
+  Widget _buildTextField({
+    required String label,
+    required TextEditingController controller,
+    required String? Function(String?) validator,
+    int maxLines = 1,
+  }) {
     return TextFormField(
       decoration: InputDecoration(
         labelText: label,
@@ -113,6 +202,8 @@ class ReciperForm extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
         ),
       ),
+      validator: validator,
+      maxLength: maxLines,
     );
   }
 }
